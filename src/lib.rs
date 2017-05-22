aeef89074caadd62859d80bf20fa02b5a5bd5396
Colin Wallace <wallacoloo@gmail.com>
2017-05-21 23:29:45 -0700

    Documentation improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,42 @@
 //! This crate provides a jagged array, i.e. a type that is semantically equivalent to
 //! `Box<[Box<[T]>]>`, but implemented with better memory locality and fewer heap allocations.
+//!
+//! # Example
+//!
+//! ```
+//! extern crate jagged_array;
+//! extern crate streaming_iterator;
+//! use std::iter::FromIterator;
+//! use jagged_array::Jagged2;
+//! use streaming_iterator::StreamingIterator;
+//!
+//! # fn main() {
+//! // Create a jagged array from a vector of vectors
+//! let mut a = Jagged2::from_iter(vec![
+//!     vec![1, 2, 3],
+//!     vec![4],
+//!     vec![],
+//!     vec![5, 6],
+//! ]);
+//!
+//! // indexing is done in (row, column) form and supports `get` and `get_mut` variants.
+//! assert_eq!(a[(1, 0)], 4);
+//! *a.get_mut((1, 0)).unwrap() = 11;
+//! assert_eq!(a.get((1, 0)), Some(&11));
+//!
+//! // Whole rows can also be accessed and modified
+//! assert_eq!(a.get_row(3), Some(&[5, 6][..]));
+//! a.get_row_mut(3).unwrap()[1] = 11;
+//! // Note that although elements are modifiable, the structure is not;
+//! // items cannot be inserted into rows, nor can new rows be added.
+//!
+//! // Iteration via `StreamingIterator`s. See the docs for more detail.
+//! let mut iter = a.stream();
+//! while let Some(row) = iter.next() {
+//!     println!("row: {:?}", row);
+//! }
+//! # }
+//! ```
 
 #[macro_use] extern crate try_opt;
 extern crate streaming_iterator;
@@ -26,6 +63,14 @@ pub struct Jagged2<T> {
     onsets: Box<[(*mut T, usize)]>,
 }
 
+/// [`StreamingIterator`] implementation for [`Jagged2`].
+/// This allows for iteration with some lifetime restrictions on the value
+/// returned by `next`. See [`Jagged2::stream`] for more info.
+///
+/// [`StreamingIterator`]: ../streaming_iterator/trait.StreamingIterator.html
+/// [`Jagged2`]: struct.Jagged2.html
+/// [`next`]: ../streaming_iterator/trait.StreamingIterator.html#method.next
+/// [`Jagged2::stream`]: struct.Jagged2.html#method.stream
 #[derive(Debug)]
 pub struct Stream<'a, T: 'a> {
     onset_iter: stream::Convert<slice::Iter<'a, (*mut T, usize)>>,
@@ -334,21 +379,27 @@ impl<T> Jagged2<T> {
     /// the lifetime of the items it yields is tied to the lifetime of the iterator
     /// itself.
     ///
-    /// Usage:
+    /// # Example
     /// ```
+    /// # extern crate jagged_array;
+    /// # extern crate streaming_iterator;
     /// use std::iter::FromIterator;
     /// use jagged_array::Jagged2;
+    /// use streaming_iterator::StreamingIterator;
+    ///
+    /// # fn main() {
     /// let a = Jagged2::from_iter(vec![
     ///     vec![1, 2, 3],
     ///     vec![4],
     /// ]);
-    /// let iter = a.stream();
+    /// let mut iter = a.stream();
     /// while let Some(row) = iter.next() {
     ///     println!("row: {:?}", row);
     /// }
+    /// # }
     /// ```
     ///
-    /// [streaming iterator](https://docs.rs/streaming-iterator)
+    /// [streaming iterator]: ../streaming_iterator/index.html
     pub fn stream<'a>(&'a self) -> Stream<'a, T> {
         Stream{ onset_iter: stream::convert(self.onsets.iter()) }
     }
