f740066179f1dc90343d15b29d05f1ebcfa8a732
Colin Wallace <wallacoloo@gmail.com>
2017-05-21 18:23:58 -0700

    Add some benchmarks

---
+++ benches/jagged2.rs
@@ -0,0 +1,54 @@
+#![feature(test)]
+
+extern crate jagged_array;
+extern crate rand;
+extern crate test;
+
+use std::iter::FromIterator;
+
+use rand::{Rng, XorShiftRng};
+use test::Bencher;
+
+use jagged_array::Jagged2;
+
+fn build_vec() -> Vec<Vec<u32>> {
+    (1..100).map(|i| {
+        (1..(4*i % 300)).collect()
+    }).collect()
+}
+
+fn build_array() -> Jagged2<u32> {
+    (1..100).map(|i| {
+        (1..(4*i % 300))
+    }).collect()
+}
+
+
+#[bench]
+fn bench_collect(b: &mut Bencher) {
+    b.iter(build_array)
+}
+
+#[bench]
+fn bench_collect_vec(b: &mut Bencher) {
+    b.iter(build_vec)
+}
+
+#[bench]
+fn bench_access(b: &mut Bencher) {
+    let mut rng = rand::XorShiftRng::new_unseeded();
+    let arr = build_array();
+    b.iter(||
+        arr.get((rng.gen::<usize>() % 128, rng.gen::<usize>() % 128))
+    );
+}
+
+#[bench]
+fn bench_access_vec(b: &mut Bencher) {
+    let mut rng = rand::XorShiftRng::new_unseeded();
+    let arr = build_vec();
+    b.iter(||
+        arr.get(rng.gen::<usize>() % 128).map(|v| v.get(rng.gen::<usize>() % 128))
+    );
+}
+
