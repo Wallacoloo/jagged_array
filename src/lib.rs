76aebb1ccf03f0884f948fd845bcef7b87564a14
Colin Wallace <wallacoloo@gmail.com>
2017-05-21 17:23:15 -0700

    Convenience functions + dealloaction correctness.
    When deallocating, the full &[T] slice type is deallocated (which
    matches exactly the type that was originally Boxed). Previously, only
    &T was deallocated, which usually works, but is not as rigidly correct.

--- src/lib.rs
+++ src/lib.rs
@@ -40,9 +40,8 @@ impl<T> Drop for Jagged2<T> {
     fn drop(&mut self) {
         unsafe {
             // Need to explicitly free memory on drop.
-            // onsets[0] points to the base address of the storage;
-            // dropping a Box created from that address will trigger deallocation.
-            Box::from_raw(self.onsets[0]);
+            // dropping a Box created from a slice of all the storage will do that.
+            Box::from_raw(self.as_flat_slice_mut() as *mut [T]);
         }
     }
 }
@@ -86,23 +85,135 @@ impl<T, ICol> FromIterator<ICol> for Jagged2<T>
 impl<T> Jagged2<T> {
     /// Index into the jagged array. The index is given in (Major, Minor) form,
     /// i.e. (row, column) or (outer, inner).
-    /// `array[(0, 0)]` is adjacent to `array[(0, 1)]` in memory but not
-    /// necessarily to `array[(1, 0)]`.
+    ///
+    /// # Example
+    /// ```
+    /// use std::iter::FromIterator;
+    /// use jagged_array::Jagged2;
+    /// let a = Jagged2::from_iter(vec![
+    ///     vec![1, 2, 3],
+    ///     vec![4],
+    ///     vec![],
+    ///     vec![5, 6],
+    /// ]);
+    /// assert!(a.get((1, 0)) == Some(&4));
+    /// assert!(a.get((2, 0)) == None);
+    /// ```
     pub fn get(&self, index: (usize, usize)) -> Option<&T> {
+        let view = try_opt!(self.get_row(index.0));
+        view.get(index.1)
+    }
+
+    /// Index into the jagged array. The index is given in (Major, Minor) form,
+    /// i.e. (row, column) or (outer, inner).
+    ///
+    /// # Example
+    /// ```
+    /// use std::iter::FromIterator;
+    /// use jagged_array::Jagged2;
+    /// let mut a = Jagged2::from_iter(vec![
+    ///     vec![1, 2, 3],
+    ///     vec![4],
+    ///     vec![],
+    ///     vec![5, 6],
+    /// ]);
+    /// assert!(a.get((1, 0)) == Some(&4));
+    /// *a.get_mut((1, 0)).unwrap() = 11;
+    /// assert!(a.get((1, 0)) == Some(&11));
+    /// ```
+    pub fn get_mut(&mut self, index: (usize, usize)) -> Option<&mut T> {
+        let view = try_opt!(self.get_row_mut(index.0));
+        view.get_mut(index.1)
+    }
+
+    /// Retrieve the given row as a contiguous slice of memory.
+    pub fn get_row(&self, row: usize) -> Option<&[T]> {
+        let (row_onset, row_len) = try_opt!(self.get_row_components(row));
+        unsafe {
+            Some(slice::from_raw_parts(row_onset, row_len))
+        }
+    }
+    /// Retrieve the given row as a contiguous slice of mutable memory.
+    pub fn get_row_mut(&mut self, row: usize) -> Option<&mut [T]> {
+        let (row_onset, row_len) = try_opt!(self.get_row_components(row));
+        unsafe {
+            Some(slice::from_raw_parts_mut(row_onset, row_len))
+        }
+    }
+
+    /// Return a slice over the entire storage area.
+    ///
+    /// # Example
+    /// ```
+    /// use std::iter::FromIterator;
+    /// use jagged_array::Jagged2;
+    /// let a = Jagged2::from_iter(vec![
+    ///     vec![1, 2, 3],
+    ///     vec![4],
+    ///     vec![],
+    ///     vec![5, 6],
+    /// ]);
+    /// assert!(a.as_flat_slice() == &vec![1, 2, 3, 4, 5, 6][..]);
+    /// ```
+    pub fn as_flat_slice(&self) -> &[T] {
+        let (addr_start, len) = self.slice_components();
+        unsafe {
+            slice::from_raw_parts(addr_start, len)
+        }
+    }
+    /// Return a mutable slice over the entire storage area.
+    ///
+    /// # Example
+    /// ```
+    /// use std::iter::FromIterator;
+    /// use jagged_array::Jagged2;
+    /// let mut a = Jagged2::from_iter(vec![
+    ///     vec![1, 2, 3],
+    ///     vec![4],
+    ///     vec![],
+    ///     vec![5, 6],
+    /// ]);
+    /// assert!(a.as_flat_slice()[3] == 4);
+    /// a.as_flat_slice_mut()[3] = 33;
+    /// assert!(a[(1, 0)] == 33);
+    /// ```
+    pub fn as_flat_slice_mut(&mut self) -> &mut [T] {
+        let (addr_start, len) = self.slice_components();
+        unsafe {
+            slice::from_raw_parts_mut(addr_start, len)
+        }
+    }
+
+    /// Return the base address of the row and the number of items in the row,
+    /// or None if the row doesn't exist.
+    ///
+    /// This method is useful in creating views of individual rows.
+    fn get_row_components(&self, row: usize) -> Option<(*mut T, usize)> {
         // Figure out which indices in the storage correspond to the
         // start and end of the selected row.
-        let row_end = *try_opt!(self.onsets.get(1+index.0));
+        //
+        // Note: if index.0 == usize::MAX, indexing is impossible;
+        // we cannot have that many rows because of the extra overhead for tracking the row's END.
+        // But usize::MAX = (-1isize as usize), so we definitely want to make
+        // sure we correctly error for that query.
+        let idx_of_row_end = try_opt!(row.checked_add(1));
+        let row_end = *try_opt!(self.onsets.get(idx_of_row_end));
         // Because onsets[1+index.0] exists, it's safe to directly access
         // onsets[index.0]
-        let row_onset = self.onsets[index.0];
+        let row_onset = self.onsets[row];
         // TODO: Can T be size 0? If so this errors.
         let row_len = (row_end as usize - row_onset as usize)/mem::size_of::<T>();
 
-        unsafe {
-            // Let the slice do the bounds checking on the column access for us.
-            let row_slice = slice::from_raw_parts(row_onset, row_len);
-            row_slice.get(index.1)
-        }
+        Some((row_onset, row_len))
+    }
 
+    /// Return the base pointer to the storage area and the number of items stored.
+    /// This is used to construct slice views of the storage.
+    fn slice_components(&self) -> (*mut T, usize) {
+        let addr_start = self.onsets[0];
+        let addr_end = *self.onsets.last().unwrap();
+        let len = (addr_end as usize - addr_start as usize)/mem::size_of::<T>();
+        (addr_start, len)
     }
 }
+
